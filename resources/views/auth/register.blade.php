5091c34d6759d1a39c268d2c916827e1bb47ee5a
nishikiyuu <yuka2000nishi@gmail.com>
2024-09-14 23:49:45 +0900

    ユーザー登録編集中(url?)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,39 @@
 <x-logout-layout>
-    <!--ユーザー登録-->
-    <!-- 適切なURLを入力してください -->
-{!! Form::open(['url' => '〇〇']) !!}
+<!--ユーザー登録-->
+<!-- 適切なURLを入力してください -->
 
-<h2>新規ユーザー登録</h2>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    </head>
+    <body>
+        {!! Form::open(['url' => '/seeders/users']) !!}
 
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
+        <h2>新規ユーザー登録</h2>
 
-{{ Form::label('メールアドレス') }}
-{{ Form::email('email',null,['class' => 'input']) }}
+        {{ Form::label('ユーザー名') }}
+        {{ Form::text('username',null,['class' => 'input']) }}
 
-{{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }}
+        {{ Form::label('メールアドレス') }}
+        {{ Form::email('email',null,['class' => 'input']) }}
 
-{{ Form::label('パスワード確認') }}
-{{ Form::text('password_confirmation',null,['class' => 'input']) }}
+        {{ Form::label('パスワード') }}
+        {{ Form::text('password',null,['class' => 'input']) }}
 
-{{ Form::submit('登録') }}
+        {{ Form::label('パスワード確認') }}
+        {{ Form::text('password_confirmation',null,['class' => 'input']) }}
 
-<p><a href="login">ログイン画面へ戻る</a></p>
+        {{ Form::submit('登録') }}
 
-{!! Form::close() !!}
+        <p><a href="login">ログイン画面へ戻る</a></p>
 
+        {!! Form::close() !!}
+
+    </body>
+</html>
 
 </x-logout-layout>
