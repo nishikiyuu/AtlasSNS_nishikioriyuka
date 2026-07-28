57dbf0f0768fc2e34fa78e413c039a6df21bf5a4
nishikiyuu <yuka2000nishi@gmail.com>
2026-07-29 00:08:20 +0900

    新規登録(register.blade)、登録後画面(added.blade)のcss編集完了

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -1,14 +1,21 @@
 <x-logout-layout>
-<!--登録完了画面-->
+  <!--登録完了画面-->
   <div id="clear">
     {!! Form::open(['url' => 'added']) !!}
-    <p>{{$store}}さん</p>
+    <div class="added_content">
+      <div class="added_group">
+        <p>{{$store}}さん</p>
+        <p>ようこそ！AtlasSNSへ</p>
+      </div>
+      <div class="added_comment">
+        <p>ユーザー登録が完了しました。</p>
+        <p>早速ログインをしてみましょう!</p>
+      </div>
 
-    <p>ようこそ！AtlasSNSへ！</p>
-    <p>ユーザー登録が完了しました。</p>
-    <p>早速ログインをしてみましょう。</p>
-
-    <p class="btn"><a href="login">ログイン画面へ</a></p>
+      <div class="added_btn">
+        <a href="login" class="btn btn-danger" role="button">ログイン画面へ</a>
+      </div>
+    </div>
     {!! Form::close() !!}
   </div>
 </x-logout-layout>
